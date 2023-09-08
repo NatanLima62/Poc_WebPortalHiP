7c403875afb54a8566f5af30ffad2fabb9c1db62
NatchanDesu <natanlimaoliveira62@gmail.com>
2023-09-08 15:42:49 -0300

    ajuste erro gost '='

--- Poc_WebPortalHiP.Api/Api/Configuration/AuthenticationConfiguration.cs
+++ Poc_WebPortalHiP.Api/Api/Configuration/AuthenticationConfiguration.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Tokens;
 using Poc_WebPortalHiP.Api.Core.Settings;
 
@@ -12,23 +13,31 @@ public static class AuthenticationConfiguration
         services.Configure<JwtSettings>(appSettingsSection);
         
         var appSettings = appSettingsSection.Get<JwtSettings>();
+        // services
+        //     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        //     .AddJwtBearer(options =>
+        //     {
+        //         options.RequireHttpsMetadata = true;
+        //         options.SaveToken = true;
+        //         options.IncludeErrorDetails = true; // <- great for debugging
+        //         options.TokenValidationParameters = new TokenValidationParameters
+        //         {
+        //             ValidateIssuer = true,
+        //             ValidateAudience = true,
+        //             ValidateLifetime = true,
+        //             ValidateIssuerSigningKey = true,
+        //             ValidIssuer = appSettings.Emissor,
+        //             ValidAudiences = appSettings.Audiences()
+        //         };
+        //     });
+        
         services
             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
+            .AddMicrosoftIdentityWebApi(options =>
             {
-                options.RequireHttpsMetadata = true;
-                options.SaveToken = true;
-                options.IncludeErrorDetails = true; // <- great for debugging
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = appSettings.Emissor,
-                    ValidAudiences = appSettings.Audiences()
-                };
-            });
+                configuration.Bind("AzureAd", options);
+                options.TokenValidationParameters.NameClaimType = "name";
+            }, options => { configuration.Bind("AzureAd", options); });
         
         services
             .AddJwksManager()
